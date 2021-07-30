256e22688ace9574c0b12efe35f41863bd30f4be
jeesoncherukattil <jeesooncherukattil@gmail.com>
2021-07-30 10:11:53 +0530

    Validation checks, fixes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function expandFun(ele, pageClass) {
     target_id = (ele.parentElement.getAttribute('for'));
-    if(ele.innerHTML == '+'){
+    if (ele.innerHTML == '+') {
         servDescArr = document.getElementsByClassName(pageClass);
         expandIconsArr = document.getElementsByClassName('plus-expand');
-        for(i = 0; i < servDescArr.length; i++){
+        for (i = 0; i < servDescArr.length; i++) {
             servDescArr[i].style.display = 'none';
             expandIconsArr[i].innerHTML = '+'
-            expandIconsArr[i].style.background = '#6d1a5c';
+            expandIconsArr[i].style.background = '#463bbb';
         }
         document.getElementById(target_id).nextElementSibling.style.display = 'block';
         ele.innerHTML = '-'
         ele.style.background = '#5c5c5c';
-    }else{
+    } else {
         document.getElementById(target_id).nextElementSibling.style.display = 'none';
         ele.innerHTML = '+'
-        ele.style.background = '#6d1a5c';
+        ele.style.background = '#463bbb';
     }
-    if(pageClass == 'career-desc'){
+    if (pageClass == 'career-desc') {
         document.getElementById(target_id).nextElementSibling.scrollIntoView(true);
     }
 }
